c59ba264a80767827d9a53d871ad52ab2fa27d05
rr37 <ay870527@gmail.com>
2023-05-29 20:03:45 +0800

    feat: initialize api server

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
-const routes = require('./routes')
+const { pages } = require('./routes')
 const { getUser } = require('./helpers/auth-helpers')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   res.locals.user = getUser(req)
   next()
 })
-app.use(routes)
+app.use(pages)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
