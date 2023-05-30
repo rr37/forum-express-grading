b131a4ce2349a558fd67ca95931dd2208485df09
rr37 <ay870527@gmail.com>
2023-05-30 10:03:08 +0800

    feat: add api error handler & delete restaurant api

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
+const { apiErrorHandler } = require('../../middleware/error-handler')
 
 // admin 總路由
 router.use('/admin', admin)
 
+// Restaurants
 router.get('/restaurants', restController.getRestaurants)
+
+// 錯誤處理
+router.use('/', apiErrorHandler)
 module.exports = router
