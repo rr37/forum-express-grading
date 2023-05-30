4ee47c558a84fb643d1896ac710a4ce83e9a10b2
rr37 <ay870527@gmail.com>
2023-05-30 14:48:56 +0800

    feat: api signin add jwt token

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
+const userController = require('../../controllers/apis/user-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
 // admin 總路由
 router.use('/admin', admin)
+
+// 註冊、登入、登出
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
 // Restaurants
 router.get('/restaurants', restController.getRestaurants)
